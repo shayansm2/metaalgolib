28c221def5d727c4d658c54269168dd2231542cf
Shayan Shafiee Moghadam <shayansm09@gmail.com>
2022-03-23 14:05:05 +0430

    init the project

---
+++ src/algorithms/lib/Algorithm.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from src.algorithms.lib.HyperParameterStorage import HyperParameterStorage
+
+
+class Algorithm(ABC):
+    def __init__(self):
+        self.hyperParameter = HyperParameterStorage()
+
+    def set_hyper_parameter(self, name, value):
+        self.hyperParameter.set_hyper_parameter(name, value)
+        return self
+
+    @abstractmethod
+    def execute(self):
+        pass  # TODO should be implemented on the child
